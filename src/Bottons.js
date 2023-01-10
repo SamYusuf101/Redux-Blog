51b496c693205f86aa533e454662b177f787b202
Sam Yusuf <samyusuf101@gmail.com>
2023-01-10 21:15:54 +0100

    Update Bottons.js

--- src/Bottons.js
+++ src/Bottons.js
@@ -8,4 +8,16 @@ import Link from "next/link";
 
   </Link>
 Link>
-</div>;
+</div>
+
+<div className="space-x-5 space-y-5 pt-80  mt-60 md:pt-80 md:mt-10 lg:pt-60  ">
+  
+  
+    <button>
+    </button>
+
+  </Link>
+Link>
+
+
+
